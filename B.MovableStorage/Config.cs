ca95d115f92c320fe15a4d31dde361e53c6467b2
Jack Morrison <jack@morrison.ec>
2022-02-25 10:06:13 +0000

    Allows you to change how many of a specific type can be picked up instead of a global amount per player.

--- B.MovableStorage/Config.cs
+++ B.MovableStorage/Config.cs
@@ -1,5 +1,6 @@
 ﻿using Rocket.API;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace B.MovableStorage
 {
@@ -8,23 +9,55 @@ namespace B.MovableStorage
         public int AmountOfStorages;
         public byte WidthOfAdminStorage;
         public byte HightOfAdminStorage;
-        public bool HitStorageToPickup;
-        public ushort Tool;
-        public List<ushort> Storages;
-
+        [XmlArray("Storages")]
+        [XmlArrayItem("Storage")]
+        public List<ConfigStorages> Storages;
 
         public void LoadDefaults()
         {
             AmountOfStorages = 3;
             WidthOfAdminStorage = 10;
             HightOfAdminStorage = 10;
-            HitStorageToPickup = false;
-            Tool = 138;
 
-            Storages = new List<ushort>()
+            Storages = new List<ConfigStorages>()
             {
-                328
+                new ConfigStorages(328, 2, new Tool(true, 138)),
+                new ConfigStorages(366, 5, new Tool(false, 138))
             };
+        }
+    }
+
+    public class ConfigStorages
+    {
+        public ushort StorageID { get; set; }
+        public int AmountAllowed { get; set; }
+        public Tool Tool { get; set; }
+
+        public ConfigStorages()
+        {
+        }
+
+        public ConfigStorages(ushort storage, int amount, Tool tool)
+        {
+            StorageID = storage;
+            AmountAllowed = amount;
+            Tool = tool;
+        }
+    }
+
+    public class Tool
+    {
+        public bool Enabled { get; set; }
+        public ushort ToolID { get; set; }
+
+        public Tool()
+        {
+        }
+
+        public Tool(bool enabled, ushort tool)
+        {
+            Enabled = enabled;
+            ToolID = tool;
         }
     }
 }
